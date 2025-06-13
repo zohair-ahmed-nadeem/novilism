d8a64566a56b04b8fde2393dfd0d71ef11507062
ZAN <zohairahmednadeemprince@gmail.com>
2025-06-13 13:50:18 +0500

    add new app which name is user, set profile button in home page

--- novilism/settings.py
+++ novilism/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'post',
     'widget_tweaks',
     'ckeditor',
+    'user',
 ]
 
 MIDDLEWARE = [
--- novilism/urls.py
+++ novilism/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', post_views.home, name='home'),
     path('story/', include('post.urls')),
-    path("markdownx/", include("markdownx.urls")),
+    path('user/', include('user.url') ),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
